82ea6ff1513986008fde7cbc565b3ea146fd61be
Camilo <cbarraza11@gmail.com>
2014-01-17 04:29:56 -0500

    nueva tabla en base de datos con migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131027140915) do
+ActiveRecord::Schema.define(:version => 20140116143829) do
+
+  create_table "pruebas", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "tunes", :force => true do |t|
     t.string   "author"
